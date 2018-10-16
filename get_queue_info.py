2a8e42234ec7f9e9a5e4aa4bd20d270523def168
unknown <wangxingwei@aspire.aspire-tech.com>
2018-10-16 14:31:43 +0800

    删除一些私人信息

--- get_queue_info.py
+++ get_queue_info.py
@@ -13,7 +13,4 @@ def get_queue_info(url, user, pas):
     return [queue_info[i:i+5] for i in range(0, len(queue_info), 5)]
 
 
-result = get_queue_info('http://192.168.61.27:8161/admin/queues.jsp',
-                        'admin', 'admin')
-print(result)
 
