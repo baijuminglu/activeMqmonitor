f7c4cd31b1018a26ca54e5da2e85307ab9e571d3
baijuminglu <wangxingwei0624@gmail.com>
2018-10-26 16:29:11 +0800

    change function argument  from  url  to ip and  port

--- get_queue_info.py
+++ get_queue_info.py
@@ -3,7 +3,8 @@ from requests.auth import HTTPBasicAuth
 from bs4 import BeautifulSoup
 
 
-def get_queue_info(url, user, pas):
+def get_queue_info(ip, port, user, pas):
+    url = "http://{0}:{1}/admin/queues.jsp".format(ip, port)
     req = requests.get(url, auth=HTTPBasicAuth(user, pas))
     soup = BeautifulSoup(req.text, 'lxml')
     table = soup.find('table', {'id': 'queues'}).find('tbody').stripped_strings
@@ -11,6 +12,7 @@ def get_queue_info(url, user, pas):
                                                  "Send To", "Purge", 'Delete',
                                                  'Active Producers'], table))
     return [queue_info[i:i+5] for i in range(0, len(queue_info), 5)]
+
 
 
 
